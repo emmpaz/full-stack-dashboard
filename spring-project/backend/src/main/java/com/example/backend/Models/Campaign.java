a6743b072e3bf28fc8928ad65c640a88823be1c3
gleverett <gal69@case.edu>
2022-07-12 15:22:17 -0500

    campaign model

--- spring-project/backend/src/main/java/com/example/backend/Models/Campaign.java
+++ spring-project/backend/src/main/java/com/example/backend/Models/Campaign.java
@@ -3,18 +3,75 @@ package com.example.backend.Models;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Campaign {
     @Id
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @Column(name = "campaignid", nullable = false)
+    private Long campaignid;
 
-    public Long getId() {
-        return id;
+    @Id
+    @Column(name = "managerid", nullable = false)
+    private Long managerid;
+
+    @Column(name = "campaignName", nullable = false, columnDefinition = "varchar(255)")
+    private String campaignName;
+
+    @Column(name = "startDate", nullable = false, columnDefinition = "DATE")
+    private Date startDate;
+
+    @Column(name = "endDate", nullable = false, columnDefinition = "DATE")
+    private Date endDate;
+
+    @Column(name = "active", nullable = false, columnDefinition = "boolean default false")
+    private Boolean active;
+
+    public Long getCampaignId() {
+        return campaignid;
+    }
+    
+    public Long getManagerId() {
+        return managerid;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getCampaignName() {
+        return campaignName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setCampaignId(Long id) {
+        this.campaignid = id;
+    }
+
+    public void setManagerId(Long id) {
+        this.managerid = id;
+    }
+
+    public void setCampaignName(String name) {
+        this.campaignName = name;
+    }
+
+    public void setStartDate(Date date) {
+        this.startDate = date;
+    }
+
+    public void setEndDate(Date date) {
+        this.endDate = date;
+    }
+    
+    public void setActivity(Boolean newActivity) {
+        this.active = newActivity;
     }
 }
