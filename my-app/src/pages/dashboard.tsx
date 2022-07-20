23618b8695f303a3f9639bd8576f6929a46001b6
Grace Leverett <gralever@publicisgroupe.net>
2022-07-20 13:45:29 -0500

    WOO

--- my-app/src/pages/dashboard.tsx
+++ my-app/src/pages/dashboard.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import { Campaign } from './types';
+
+const get_campaigns = axios.create({
+    baseURL: 'https://ps-springboot.azurewebsites.net/campaign'
+})
 
 const Dashboard = () => {
     const navigate = useNavigate();
+    const [myCampaigns, setCampaigns] = useState<Campaign[]>([]);
+
+useEffect(() => {
+    fetchCampaigns();}, []
+);
+
+const fetchCampaigns = () => {
+    axios.get('https://ps-springboot.azurewebsites.net/campaign').then((res) => {
+      console.log(res);
+
+      setCampaigns(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+
+
+  /*  componentDidMount() {
+        get_campaigns.get('/').then(res => {
+            console.log(res.data.campaignName)
+        })
+    }*/
+    
 
     return(
         <div>
@@ -13,6 +43,14 @@ const Dashboard = () => {
                 type = "text"
                 placeholder = "search"
                 ></input>
+            <h1>List of Campaigns</h1>
+            <div className='camp-container'>
+                {myCampaigns.map((campaign) => (
+                    <div className='card'>
+                        <h3>{campaign.campaignName}</h3>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
---
+++ my-app/src/pages/types.tsx
@@ -0,0 +1,12 @@
+
+export type Campaign = {
+    managerId: number;
+    banner: String;
+    channel: String;
+    company: String;
+    budget: number;
+    campaignName: String;
+    startDate: Date;
+    endDate: Date;
+    isActive: Boolean;
+}
