154c139aeee03c4653dd8873d35394638a2b31df
Grace Leverett <gralever@publicisgroupe.net>
2022-07-21 14:07:53 -0500

    WORKING ADD CAMPAIGN !!!

--- my-app/src/components/createCampaignComp.tsx
+++ my-app/src/components/createCampaignComp.tsx
@@ -1,47 +1,33 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Input } from '@mui/material';
-import { Campaign } from '../pages/types';
+import { Box, Button, Input, TextField } from '@mui/material';
+import { Campaign } from '../helper files/types';
+
+const defaultCampaign: Campaign = {
+    managerId: 123,
+    banner: '',
+    company: '',
+    channel: '',
+    budget: 10000.00,
+    campaignName: '',
+    startDate: new Date(2022-12-12),
+    endDate: new Date(2023-1-12),
+    isActive: true,
+}
 
 export const CreateCampaignComp = () => {
-    const newCampaign: Campaign = {
-        managerId: 123,
-        banner: '',
-        company: '',
-        channel: '',
-        budget: 10000.00,
-        campaignName: '',
-        startDate: new Date(2022-12-12),
-        endDate: new Date(2023-1-12),
-        isActive: true,
-    }
-    
-    // const {
-    //     managerId: 123,
-    //     banner: '',
-    //     company: '',
-    //     channel: '',
-    //     budget: 10000.00,
-    //     campaignName: '',
-    //     startDate: 2022-12-12,
-    //     endDate: 2023-1-12,
-    //     isActive: true,
-    // }
+    const [newCampaign, setNewCampaign] = useState<Campaign>(defaultCampaign)
+
+    const handleInputChange = (e: any) => {
+        const { name, value } = e.target;
+        setNewCampaign({
+          ...newCampaign,
+          [name]: value,
+        });
+      };
 
     const handleSubmit = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
-
-    //    var campaign = {
-    //         managerId: this.state.managerId,
-    //         banner: this.state.banner,
-    //         company: this.state.company,
-    //         channel: this.state.channel,
-    //         budget: this.state.budget,
-    //         campaignName: this.state.campaignName,
-    //         startDate: this.state.startDate,
-    //         endDate: this.state.endDate,
-    //         isActive: this.state.isActive,
-    //     };
 
         axios.post('https://ps-springboot.azurewebsites.net/campaign', newCampaign)
             .then(res => {
@@ -51,33 +37,22 @@ export const CreateCampaignComp = () => {
     }
     
     return (
-        <div>
-            <label>
-                Campaign Name:
-                <Input type="text" name="campaignName" placeholder="Campaign Name"  value={newCampaign.campaignName}></Input>
-            </label><br/>
-            <label>
-                Client Name:
-                <Input type="text" name="company" placeholder="Client Name"></Input>
-            </label><br/>
-            <label>
-                Channel:
-                <Input type="text" name="channel" placeholder="Channel"></Input>
-            </label><br/>
-            <label>
-                Budget:
-                <Input type="number" name="budget" placeholder="1000.00"></Input>
-            </label><br/>
-            <label>
-                Start Date:
-                <Input type="date" name="startDate"></Input>
-            </label><br/>
-            <label>
-                End Date:
-                <Input type="date" name="endDate"></Input>
-            </label><br/>
+        <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+            <Input type="text" name="campaignName" placeholder="Campaign Name" value={newCampaign.campaignName} onChange={handleInputChange}></Input><br />
+            <Input type="text" name="company" placeholder="Client Name" value={newCampaign.company} onChange={handleInputChange}></Input><br />
+            <Input type="text" name="channel" placeholder="Channel" value={newCampaign.channel} onChange={handleInputChange}></Input><br />
+            <Input type="number" name="budget" placeholder="1000.00" value={newCampaign.budget} onChange={handleInputChange}></Input><br />
+            <Input type="date" name="startDate" value={newCampaign.startDate} onChange={handleInputChange}></Input><br />
+            <Input type="date" name="endDate" value={newCampaign.endDate} onChange={handleInputChange}></Input><br />
             <Button type="submit" onClick={handleSubmit}>Submit</Button>
-        </div>
+        </Box>
     )
 }
 
