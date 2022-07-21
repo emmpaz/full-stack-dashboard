c8f704b366cb871e3ad1d06794b06cf3f4890c4a
Emmanuel Paz <emmanuel.paz@publicissapient.com>
2022-07-21 15:22:36 -0500

    sorting features

--- my-app/src/helper files/comparators.tsx
+++ my-app/src/helper files/comparators.tsx
@@ -4,8 +4,19 @@ export const compare_by_name = (x: Campaign, y: Campaign) => {
     return x.campaignName.localeCompare(y.campaignName.toString())
 }
 
+export const compare_by_name_reversed = (x: Campaign, y: Campaign) => {
+    return y.campaignName.localeCompare(x.campaignName.toString())
+}
+
 export const compare_by_date = (x: Campaign, y: Campaign) => {
     if(x.endDate <= y.endDate)
+        return 1
+    else
+        return -1
+}
+
+export const compare_by_date_reversed = (x: Campaign, y: Campaign) => {
+    if(x.endDate > y.endDate)
         return 1
     else
         return -1
--- my-app/src/pages/dashboard.tsx
+++ my-app/src/pages/dashboard.tsx
@@ -4,16 +4,33 @@ import axios from 'axios';
 import { Campaign } from '../helper files/types';
 import {Button} from "@mui/material";
 import {useIsMount} from '../helper files/mounting';
-import { compare_by_date, compare_by_name, compare_by_budget} from '../helper files/comparators';
+import { compare_by_date, compare_by_name, compare_by_budget, compare_by_name_reversed, compare_by_date_reversed} from '../helper files/comparators';
 import { CampaignList } from '../components/campaignList';
 
 const get_campaigns = axios.create({
     baseURL: 'https://ps-springboot.azurewebsites.net/campaign'
 })
 
+const sortNameState = [
+    "A-Z",
+    "Z-A",
+    "default"
+]
+
+const sortDateState = [
+    "earliest",
+    "latest",
+    "default"
+]
+
 const Dashboard = () => {
     const navigate = useNavigate();
     const [myCampaigns, setCampaigns] = useState<Campaign[]>([]);
+    const [originalList, setList] = useState<Campaign[]>([]);
+
+    const [sortName, setName] = useState<String>(sortNameState[2]);
+    const [sortDate, setDate] = useState<String>(sortDateState[2]);
+
     const isMount = useIsMount();
 
     useEffect(() => {
@@ -28,7 +45,7 @@ const Dashboard = () => {
     const fetchCampaigns = () => {
         axios.get('https://ps-springboot.azurewebsites.net/campaign').then((res) => {
         console.log(res);
-
+        setList(res.data);
         setCampaigns(res.data);
         })
         .catch((err) => {
@@ -37,29 +54,58 @@ const Dashboard = () => {
     };
 
     const sortNameHandler = () => {
+        setDate(sortDateState[2]);
+
         const list: Campaign[] = [...myCampaigns]
-        list.sort(compare_by_name);
-        setCampaigns(list);
+        if(sortName === "default"){
+            setName(sortNameState[0]);
+            list.sort(compare_by_name);
+            setCampaigns(list);
+        }
+        else if(sortName === "A-Z"){
+            setName(sortNameState[1]);
+            list.sort(compare_by_name_reversed);
+            setCampaigns(list);
+        }
+        else{
+            setName(sortNameState[2]);
+            setCampaigns(originalList);
+        }
     }
 
     const sortEndDateHandler = () => {
+        setName(sortNameState[2])
+
         const list: Campaign[] = [...myCampaigns]
-        list.sort(compare_by_date);
-        setCampaigns(list);
+        if(sortDate === "default"){
+            setDate(sortDateState[0]);
+            list.sort(compare_by_date);
+            setCampaigns(list);
+        }
+        else if(sortDate === "earliest"){
+            setDate(sortDateState[1]);
+            list.sort(compare_by_date_reversed);
+            setCampaigns(list);
+        }
+        else{
+            setDate(sortDateState[2]);
+            setCampaigns(originalList);
+        }
     }
 
     const sortBudgetHandler = () => {
         const list: Campaign[] = [...myCampaigns]
         list.sort(compare_by_budget);
         setCampaigns(list);
+        setName(sortNameState[2])
     }
     return(
         <div>
         <Button style={{float: 'right'}} onClick={() => navigate("/login")}>Logout</Button>
             <h1>List of Campaigns</h1>
             <Button onClick={() => navigate("/createCampaign")}>Create Campaign</Button>
-            <Button onClick={sortNameHandler} style={{margin: 21}}>A-Z</Button>
-           <Button  onClick={sortEndDateHandler} style={{margin: 21}}>end date</Button>
+            <Button variant={(sortName === "A-Z" || sortName === "Z-A") ? "contained": "text"} onClick={sortNameHandler} style={{margin: 21}}>{(sortName === "default") ? "A-Z" : sortName}</Button>
+           <Button  variant={(sortDate === "earliest" || sortDate === "latest") ? "contained": "text"} onClick={sortEndDateHandler} style={{margin: 21}}>{(sortDate === "default") ? "end DATE" + String.fromCharCode(8593) : sortDate}</Button>
            <Button  onClick={sortBudgetHandler} style={{margin: 21}}>SPEND</Button>
             <div className='camp-container'>
                 {myCampaigns.map((campaign) => (
