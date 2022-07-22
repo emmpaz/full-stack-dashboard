7439ec4fd577bedb19aeb083c8d59a152a1aca9d
Emmanuel Paz <emmanuel.paz@publicissapient.com>
2022-07-22 14:06:19 -0500

    archived and active buttons are implemented

--- my-app/src/pages/dashboard.tsx
+++ my-app/src/pages/dashboard.tsx
@@ -39,26 +39,30 @@ const sortSpendState = [
 const Dashboard = () => {
     const navigate = useNavigate();
 
+    //campaign lists states
     const [myCampaigns, setCampaigns] = useState<Campaign[]>([]);
     const [originalList, setList] = useState<Campaign[]>([]);
 
+    //sorting states
     const [sortName, setName] = useState<String>(sortNameState[2]);
     const [sortDate, setDate] = useState<String>(sortDateState[2]);
     const [sortSpend, setSpend] = useState<String>(sortSpendState[2]);
 
+    //active and archived states
+    const [isActive, setActive] = useState<Boolean>(true);
     const isMount = useIsMount();
 
     useEffect(() => {
         if (isMount) {
-            fetchCampaigns();
+            fetchCampaigns("active");
             console.log('fetching');
         } else {
         console.log('Subsequent Render');
         }
   });
 
-    const fetchCampaigns = () => {
-        axios.get('https://ps-springboot.azurewebsites.net/campaign').then((res) => {
+    const fetchCampaigns = (active_or_not: String) => {
+        axios.get(`https://ps-springboot.azurewebsites.net/${active_or_not}_campaigns`).then((res) => {
         console.log(res);
         setList(res.data);
         setCampaigns(res.data);
@@ -67,6 +71,36 @@ const Dashboard = () => {
         console.log(err);
         });
     };
+
+    const archivedCampaignsHandler = () => {
+        if(!isActive){
+            setDate(sortDateState[2])
+            setName(sortNameState[2])
+            setSpend(sortSpendState[2])
+            
+            setActive(!isActive)
+            fetchCampaigns("active")
+        }
+        else{
+            setDate(sortDateState[2])
+            setName(sortNameState[2])
+            setSpend(sortSpendState[2])
+
+            setActive(!isActive)
+            fetchCampaigns("archived")
+        }
+    }
+
+    const activeCampaignsHandler = () => {
+        if(!isActive){
+            setDate(sortDateState[2])
+            setName(sortNameState[2])
+            setSpend(sortSpendState[2])
+
+            setActive(!isActive)
+            fetchCampaigns("active")
+        }
+    }
 
     const sortNameHandler = () => {
         setDate(sortDateState[2]);
@@ -140,6 +174,8 @@ const Dashboard = () => {
                 <CampaignContainer>
                     <div>
                         <h1>List of Campaigns</h1>
+                        <Button variant={(isActive) ? "contained" : "text"} onClick={activeCampaignsHandler}>Active</Button>
+                        <Button variant={(!isActive) ? "contained" : "text"} onClick={archivedCampaignsHandler}>Archived</Button>
                         <Button onClick={() => navigate("/createCampaign")}>Create Campaign</Button>
                         <Button variant={(sortName === "A-Z" || sortName === "Z-A") ? "contained": "text"} onClick={sortNameHandler} style={{margin: 21}}>{(sortName === "default") ? "A-Z" : sortName}</Button>
                         <Button variant={(sortDate === end_date_down || sortDate === end_date_up) ? "contained": "text"} onClick={sortEndDateHandler} style={{margin: 21}}>{(sortDate === "default") ? end_date_down : sortDate}</Button>
