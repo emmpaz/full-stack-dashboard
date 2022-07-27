17c66177d0f2650fc50a10fd5fd20a3810342f71
Grace Leverett <gralever@publicisgroupe.net>
2022-07-27 15:04:27 -0500

    more preview updates

--- my-app/src/pages/detailView.tsx
+++ my-app/src/pages/detailView.tsx
@@ -1,6 +1,7 @@
-import { Box, Paper, Typography, BoxProps } from '@mui/material';
+import { Box, Paper, Typography, BoxProps, Grid } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { CampaignContainer, GraphContainer } from '../components/containers';
 import { CampListItem } from '../components/func_camp_list';
 import { Campaign } from '../helper files/types';
 
@@ -16,27 +17,28 @@ const DetailView = () => {
     
     return(
         <div>
-            <Paper elevation={3} sx={{borderRadius: 5}}>
-            <Box p={3}>
-                <Typography variant='h4'>
-                    {campaign.currentCamp.campaignName}
-                </Typography>           
-                <Typography variant='h5'>
-                    {campaign.currentCamp.company}
-                </Typography>
-                <Typography variant='body1'>
-                    {campaign.currentCamp.startDate} {' '} to {' '} {campaign.currentCamp.endDate}
-                </Typography>
-                <Typography variant='h6' color='#00C832 !important'>
-                    Budget: {' '} {campaign.currentCamp.budget}
-                </Typography>
-            </Box></Paper>
-            <Paper>
-                <Box>
-                    <p>slay</p>
-                </Box>
-            </Paper>
+            <Grid container direction="row" spacing={20} sx={{ flexGrow: 1 }}>
+                <Paper elevation={3} sx={{borderRadius: 5}}>
+                    <Box p={3}>
+                        <Typography variant='h5'>{campaign.currentCamp.campaignName}</Typography>           
+                        <Typography variant='h6'>{campaign.currentCamp.company}</Typography>
+                        <Typography variant='body2'>{campaign.currentCamp.startDate} {' '} to {' '} {campaign.currentCamp.endDate}</Typography>
+                        <Typography variant='body1' color='#00C832 !important'>Budget: {' '} {campaign.currentCamp.budget}</Typography>
+                    </Box>
+                </Paper>
+                <Paper elevation={3} sx={{borderRadius: 5}}>
+                    <Box p={3}>
+                        <Typography variant='h5'>Preview</Typography>
+                    </Box>
+                </Paper>
+                <Paper elevation={3} sx={{borderRadius: 5}}>
+                    <Box p={3}>
+                        <Typography variant='h5'>{campaign.currentCamp.company}</Typography>
+                    </Box>
+                </Paper>
+            </Grid>
         </div>
+        
     )
 }
 
