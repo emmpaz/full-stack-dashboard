d805c2373a52567373d91ca57ecdf2bcf647f3e5
Grace Leverett <gralever@publicisgroupe.net>
2022-07-27 15:49:01 -0500

    detail view

--- my-app/src/pages/detailView.tsx
+++ my-app/src/pages/detailView.tsx
@@ -1,5 +1,6 @@
-import { Box, Paper, Typography, BoxProps, Grid } from '@mui/material';
-import React, { useEffect } from 'react';
+import { Box, Paper, Typography, BoxProps, Grid, checkboxClasses } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { CampaignContainer, GraphContainer } from '../components/containers';
 import { CampListItem } from '../components/func_camp_list';
@@ -8,10 +9,37 @@ import { Campaign } from '../helper files/types';
 const DetailView = () => {
     const { state } = useLocation();
     const campaign = state as any;
+    const [campaigns, setCampaigns] = useState<Campaign[]>([]);
+
+    var clientAdRev = 0;
+    var calculated = false;
+
+    function calculateClientRevenue(): number {
+        campaigns.forEach( (element) => {
+            if(element.company == campaign.currentCamp.company) {
+                clientAdRev = clientAdRev + element.budget;
+            }
+        })
+        calculated = true;
+        return clientAdRev;
+    }
+
+    const fetchCampaigns = () => {
+        axios.get('https://ps-springboot.azurewebsites.net/campaign').then((res) => {
+        //console.log(res);
+        setCampaigns(res.data);
+        })
+        .catch((err) => {
+        //console.log(err);
+        });
+       /* if(calculated == false) {
+            calculateClientRevenue();
+        }*/
+    };
     
     useEffect(() => {
        // console.log(campaign.currentCamp);
-        console.log(campaign.currentCamp.campaignName);
+        fetchCampaigns();
     })
     
     
@@ -23,7 +51,7 @@ const DetailView = () => {
                         <Typography variant='h5'>{campaign.currentCamp.campaignName}</Typography>           
                         <Typography variant='h6'>{campaign.currentCamp.company}</Typography>
                         <Typography variant='body2'>{campaign.currentCamp.startDate} {' '} to {' '} {campaign.currentCamp.endDate}</Typography>
-                        <Typography variant='body1' color='#00C832 !important'>Budget: {' '} {campaign.currentCamp.budget}</Typography>
+                        <Typography variant='body1' color='#00C832 !important'>Budget: {' $'}{campaign.currentCamp.budget}</Typography>
                     </Box>
                 </Paper>
                 <Paper elevation={3} sx={{borderRadius: 5}}>
@@ -34,6 +62,10 @@ const DetailView = () => {
                 <Paper elevation={3} sx={{borderRadius: 5}}>
                     <Box p={3}>
                         <Typography variant='h5'>{campaign.currentCamp.company}</Typography>
+                        <Typography></Typography>
+                    </Box>
+                    <Box p={3}>
+                        <Typography variant='h5'>{'$'}{calculateClientRevenue()}</Typography>
                     </Box>
                 </Paper>
             </Grid>
